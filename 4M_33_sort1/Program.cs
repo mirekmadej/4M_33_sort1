b93a07dc6d1e6ae817e23188baafe6daff127db5
Mirek Madej <m.madej@zsp3zamosc.pl>
2024-04-15 12:29:33 +0200

    tablicqa od 0

--- 4M_33_sort1/Program.cs
+++ 4M_33_sort1/Program.cs
@@ -7,9 +7,9 @@
         public Tablica(int rozmiar)
         {
             N = rozmiar;
-            tab = new int[N+1];
+            tab = new int[N];
             Random r = new Random();
-            for (int i = 1; i <= N; i++)
+            for (int i =1 ; i < N; i++)
                 tab[i] = r.Next(0,100);
             sortuj();
         }
@@ -18,7 +18,7 @@
             for (var j = 1; j < N; j++)
             {
                 var pmin = j;
-                for(var i = j+1;i <= N; i++)
+                for(var i = j+1;i < N; i++)
                 {
                     if (tab[i] < tab[pmin])
                         pmin = i;
@@ -31,7 +31,7 @@
         public override string ToString()
         {
             string s = "";
-            for(var i=1; i<= N; i++)
+            for(var i=0; i< N; i++)
                 s += tab[i].ToString() + "; ";
             return $"{s}";
         }
